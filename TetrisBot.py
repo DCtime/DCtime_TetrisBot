71d474a5402ed85705e9d997de6e8305a4381ea3
DCtime <daniel.chu916@gmail.com>
2021-04-07 14:58:14 +0800

    Fertilization Function Finished

--- TetrisBot.py
+++ TetrisBot.py
@@ -3,6 +3,26 @@
 
 import random
 
+# 將兩組2n基因交配，回傳孩子的基因
+def Fertilization(firstListOfGene, secondListOfGene):
+    answerGene = []
+    
+    if (len(firstListOfGene) != len(secondListOfGene)):
+        print("Two lists of genes must be the same length")
+        return null
+
+    firstProcessingGene = firstListOfGene
+    secondProcessingGene = secondListOfGene
+
+    firstProcessingGene = Meiosis(firstProcessingGene)
+    secondProcessingGene = Meiosis(secondProcessingGene)
+    
+    for i in range(len(firstProcessingGene)):
+        answerGene.append(firstProcessingGene[i] + secondProcessingGene[i])
+
+    return answerGene
+
+# 將一個2n的基因減數分裂，回傳一個配子的1n基因片段
 def Meiosis(listOfGene):
     answerGene = []
     processingGene = listOfGene
@@ -12,7 +32,8 @@ def Meiosis(listOfGene):
         answerGene.append(Meiosis_Allele(i))
 
     return answerGene
-    
+
+# 將一組等位基因做減數分裂(被Meiosis使用)
 def Meiosis_Allele(gene):
     
     randomNum = random.random()
@@ -31,4 +52,4 @@ def Meiosis_Allele(gene):
         print("Error, gene must be 0, 1 or 2")
     
 
-print(Meiosis([1,1,2,1,2,0,1,2,0,2,1]))
+print(Fertilization([0], [0]))
