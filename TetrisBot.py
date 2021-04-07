7db13d6c8bdf65c43fdfa39781e080e10a948643
DCtime <daniel.chu916@gmail.com>
2021-04-07 12:00:44 +0800

    start coding
    ah this is difficult

--- TetrisBot.py
+++ TetrisBot.py
@@ -3,4 +3,15 @@
 
 import pyautogui
 
-
+def CombineGamete(int firstProbability. int secondProbability):
+    finalResult = -1
+    
+    # 如果是0, 為aa X aa (0 X 0)
+    # 如果是1, 為aa X Aa, Aa X aa (0 X 1 or 1 X 0)
+    # 如果是2, 為AA X aa, aa X AA (2 X 0 or 0 X 2)
+    # 如果是S, 為Aa X Aa (1 X 1)
+    # 如果是4, 為AA X AA (2 X 2)
+    
+    return 0
+    return 1
+    return 2
