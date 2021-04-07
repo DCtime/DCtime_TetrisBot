002502230f6c061286630143b9b3eba50e8bd8be
DCtime <daniel.chu916@gmail.com>
2021-04-07 12:04:35 +0800

    added switch condition

--- TetrisBot.py
+++ TetrisBot.py
@@ -5,12 +5,25 @@ import pyautogui
 
 def CombineGamete(int firstProbability. int secondProbability):
     finalResult = -1
+    realFirstProbability = -1
+    realSecondProbability = -1
+
+    switch(firstProbability):
+        case 0:
+            realFirstProbability = 0
+            break
+        case 1:
+            realFirstProbability = 0.5
+            break
+        case 2:
+            realFirstProbability = 1
+            break
     
-    # 如果是0, 為aa X aa (0 X 0)
-    # 如果是1, 為aa X Aa, Aa X aa (0 X 1 or 1 X 0)
-    # 如果是2, 為AA X aa, aa X AA (2 X 0 or 0 X 2)
-    # 如果是S, 為Aa X Aa (1 X 1)
-    # 如果是4, 為AA X AA (2 X 2)
+    # 如果是X, 為aa X aa (0 X 0)
+    # 如果是X, 為aa X Aa, Aa X aa (0 X 1 or 1 X 0)
+    # 如果是X, 為AA X aa, aa X AA (2 X 0 or 0 X 2)
+    # 如果是X, 為Aa X Aa (1 X 1)
+    # 如果是X, 為AA X AA (2 X 2)
     
     return 0
     return 1
